037db0d1babc0b637a2d04bba953bd6bb5b4e33f
bauyrzhanabdi <bauyrzhan.abdi@nu.edu.kz>
2024-11-13 15:43:22 +0500

    update version to 14

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SuiKit",
-    platforms: [.iOS(.v17), .macOS(.v11), .watchOS(.v10), .tvOS(.v17)],
+    platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v10), .tvOS(.v17)],
     products: [
         .library(
             name: "SuiKit",
